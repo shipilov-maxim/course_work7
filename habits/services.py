95aee125b7f9af96115b5b937a6b7b7392ba3daf
Maxim Shipilov <skam3840@gmail.com>
2024-06-01 11:09:09 +0400

    schedul

--- habits/services.py
+++ habits/services.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from datetime import datetime as dt
 from django.utils import timezone
@@ -5,13 +7,15 @@ from django_celery_beat.models import IntervalSchedule, PeriodicTask, CrontabSch
 from django_celery_beat.utils import make_aware
 
 from config.settings import TELEGRAM_API
+from habits.models import Habit
 
 periodicity = {
-        1: IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.DAYS),
-        2: CrontabSchedule.objects.get_or_create(day_of_week='1-5'),
-        3: CrontabSchedule.objects.get_or_create(day_of_week='6-7'),
-        4: IntervalSchedule.objects.get_or_create(every=7, period=IntervalSchedule.DAYS),
-    }
+    1: IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.SECONDS),
+    2: CrontabSchedule.objects.get_or_create(day_of_week='1-5'),
+    3: CrontabSchedule.objects.get_or_create(day_of_week='6-7'),
+    4: IntervalSchedule.objects.get_or_create(every=7, period=IntervalSchedule.DAYS),
+}
+
 
 def send_tg_message(message, chat_id):
     """    Отправка сообщения в Telegram    """
@@ -26,13 +30,15 @@ def send_tg_message(message, chat_id):
         print(f"Произошла непредвиденная ошибка: {e}")
 
 
-def remind_of_habit(habit):
+def remind_of_habit(habit_id):
     """
     Напоминание о выполнении привычки
     """
+    habit = Habit.objects.get(pk=habit_id)
     if habit.user.chat_id:
-        message = f'''Привет!\nНе забудь сегодня выполнить привычку:"{habit.action}" в {habit.time.strftime("%H:%M")}'''
-        '''Место: {habit.place}'''
+        message = f'''Привет!\nНе забудь сегодня выполнить привычку: '''
+        f''' "{habit.action}" в {habit.time.strftime("%H:%M")}'''
+        f'''Место: {habit.place}'''
         send_tg_message(message, habit.user.chat_id)
 
 
@@ -40,14 +46,33 @@ def create_periodic_task(habit):
     """
     Создание периодической задачи Celery
     """
-    start = timezone.now().strftime("%d.%m.%Y") + habit.time.strftime(" %H:%M:%S")
-    star = dt.strptime(start, '%d.%m.%Y %H:%M:%S')
-    sta = make_aware(star)
-
+    # start_time = timezone.now().strftime("%d.%m.%Y") + habit.time.strftime(" %H:%M:%S")
+    # start_time_dt = dt.strptime(start_time, '%d.%m.%Y %H:%M:%S')
+    # aware_start_time_dt = make_aware(start_time_dt)
+    #
+    # PeriodicTask.objects.create(
+    #     interval=periodicity[habit.periodicity][0] if habit.periodicity in (1, 4) else None,
+    #     crontab=periodicity[habit.periodicity][0] if habit.periodicity in (2, 3) else None,
+    #     name=f'{habit.pk}',
+    #     task='habits.tasks.habit_track',
+    #     args=[habit.pk],
+    #     kwargs=json.dumps({
+    #         'habit_id': habit.pk,
+    #     }),
+    #     start_time=timezone.now()
+    #     # start_time=aware_start_time_dt
+    # )
+    schedule, created = IntervalSchedule.objects.get_or_create(
+        every=5,
+        period=IntervalSchedule.SECONDS,
+    )
     PeriodicTask.objects.create(
-        interval=periodicity[habit.periodicity][0] if habit.periodicity in (1, 4) else None,
-        crontab=periodicity[habit.periodicity][0] if habit.periodicity in (2, 3) else None,
-        name=f'{habit.pk}',
-        task='habits.tasks.habit_reminder',
-        start_time=sta
+        interval=schedule,
+        name=f'Send Habit {habit.pk} notification',
+        task='habits.tasks.habit_track',
+        args=[habit.pk],
+        kwargs=json.dumps({
+            'habit_id': habit.pk,
+        }),
+        start_time=timezone.now()
     )
--- habits/tasks.py
+++ habits/tasks.py
@@ -4,8 +4,8 @@ from habits.services import remind_of_habit
 
 
 @shared_task
-def habit_track(habit):
+def habit_track(habit_id):
     """
     Таск Celery для напоминания о выполнении привычки
     """
-    remind_of_habit(habit)
+    remind_of_habit(habit_id)
